d437c5df587e9e43c58819b2cb1935f88b308be1
Shamaildish <shamai0704@gmail.com>
2020-11-29 18:40:33 +0200

    fixed names and adding threads

---
+++ src/cmake-build-debug/CMakeFiles/DKVS.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/edan/workspace/DKVS/src/Client.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/Client.cpp.o"
+  "/home/edan/workspace/DKVS/src/Connection.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/Connection.cpp.o"
+  "/home/edan/workspace/DKVS/src/ConssistingHash.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/ConssistingHash.cpp.o"
+  "/home/edan/workspace/DKVS/src/Function.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/Function.cpp.o"
+  "/home/edan/workspace/DKVS/src/Hash.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/Hash.cpp.o"
+  "/home/edan/workspace/DKVS/src/LoadBalancer.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/LoadBalancer.cpp.o"
+  "/home/edan/workspace/DKVS/src/Server.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/Server.cpp.o"
+  "/home/edan/workspace/DKVS/src/main.cpp" "/home/edan/workspace/DKVS/src/cmake-build-debug/CMakeFiles/DKVS.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/cmake-build-debug/CMakeFiles/DKVS.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DKVS.dir/Client.cpp.o"
+  "CMakeFiles/DKVS.dir/Connection.cpp.o"
+  "CMakeFiles/DKVS.dir/ConssistingHash.cpp.o"
+  "CMakeFiles/DKVS.dir/Function.cpp.o"
+  "CMakeFiles/DKVS.dir/Hash.cpp.o"
+  "CMakeFiles/DKVS.dir/LoadBalancer.cpp.o"
+  "CMakeFiles/DKVS.dir/Server.cpp.o"
+  "CMakeFiles/DKVS.dir/main.cpp.o"
+  "DKVS"
+  "DKVS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DKVS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
